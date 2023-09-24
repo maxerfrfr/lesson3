c013c0b5e7e349e8b3124e2e69f48b566feff3f3
Impact <liviudulap000@gmail.com>
2023-09-24 11:02:10 +0300

    main

---
+++ main.py
@@ -0,0 +1,144 @@
+#var = 10
+
+#array = [ "Info" , "Physics", "General Culture"]
+#int_array = [1, 2, 3, 4, 5]
+#float_array = [1.3, -2, 3.14, 4.7, 5.002]
+#bool_array = [True, False, True, False]
+
+#array.append("English") #append is for adding smth
+
+#array = sorted(array) # sorted is used for sorting
+#array.sort() # sort is used to sort list
+#int_array.sort(reverse=True)  # sort(reverse=True) display
+
+#print(int_array)  #print list
+#print(sorted(int_array)) #print sorted list
+
+
+
+
+ #list = []
+ #list_lenght = int(input("Enter list Lenght\n"))
+
+
+ #for i in range(0, list_lenght):
+     #element = int(input("Enter Element\n"))
+     #list.append(element)
+ #else:
+ #    print(list)
+    # print(list.sort())
+#     print(list.sort(reverse=True))
+
+
+
+
+
+
+
+
+#list = [12, 56, 2465, 547, 245, 876]
+#print(list)
+#for i in range(0, len(list)):
+    #print(list[i]) # print list by index
+#for e in list:
+    #print (e) # print list by element
+#if 56 in list:
+    #print(True)
+#else:
+    #print(False)
+
+
+
+
+
+
+#tuple = ("A", "B", "C", "D")
+
+#print(tuple + ("E", "Finish"))
+
+
+#int_array = [1, 2, 3, 1, 5, 2, 6, 7]
+#int_array = set(int_array)
+#set_array = [1, 1, 2, 3, 1, 5, 2, 6, 7]
+#print(set_array)
+#print(int_array)
+
+
+
+#dex = {"Cod": "Python" , "Extend": ".py" , "Less":  "3"}
+
+#rint(dex.get("Cod"))
+
+#dex.update({"Less": 3})
+
+#print(dex)
+
+#for i in dex:
+    #print(1)
+    #print(dex.get(i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
